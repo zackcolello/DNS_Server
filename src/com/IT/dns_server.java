bc7ae8cda0a7103b5f1c070b112c67a75c46cb8b
zackcolello <zackcolello@gmail.com>
2016-03-31 14:56:01 -0400

    Beginning file parsing

--- src/com/IT/dns_server.java
+++ src/com/IT/dns_server.java
@@ -1,10 +1,16 @@
 package com.IT;
 
-public class Main {
+public class dns_server {
 
     public static void main(String[] args) {
 
         System.out.println("Initial Test");
         System.out.println("Daivik Sheth");
+
+
+        if(args.length == 2 && args[0].equals("-f")){
+
+        }
+
     }
 }
