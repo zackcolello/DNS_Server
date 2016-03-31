71ef36b00ec2d933820f9fc009c65a2444ce1f91
zackcolello <zackcolello@gmail.com>
2016-03-31 15:43:14 -0400

    Created method to build hashmap from input DNS host file

--- src/com/IT/dns_server.java
+++ src/com/IT/dns_server.java
@@ -1,14 +1,70 @@
 package com.IT;
 
+import java.util.*;
+import java.io.*;
+
 public class dns_server {
 
-    public static void main(String[] args) {
+    private static HashMap<String, String> dnsTable;
 
-        System.out.println("Initial Test");
-        System.out.println("Daivik Sheth");
+    public static void main(String[] args) throws IOException{
 
 
-        if(args.length == 2 && args[0].equals("-f")){
+        FileInputStream fs = null;
+
+            if(args.length == 2 && args[0].equals("-f")){
+                fs = new FileInputStream(args[1]);
+            }else if (args.length == 4 && args[0].equals("-f")){
+                fs = new FileInputStream(args[1]);
+            } else if (args.length == 4 && args[2].equals("-f")) {
+                fs = new FileInputStream(args[3]);
+            }
+
+        if(fs != null) {
+            createDnsTable(fs);
+        }
+    }
+
+    private static void createDnsTable(FileInputStream fs) throws IOException{
+
+        dnsTable = new HashMap<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(fs));
+
+        String line;
+        while((line = br.readLine()) != null){
+
+            line = line.trim();
+
+            // Ignore blank lines
+            if(line.length() < 1){
+                continue;
+            }
+
+            // Ignore comments
+            if(line.charAt(0) == '#'){
+                continue;
+            }else if(line.indexOf('#') != -1){
+                line = line.substring(0, line.indexOf('#'));
+                line = line.trim();
+            }
+
+            // Divide word into IP and Domain Name
+            int separator = line.indexOf(' ');
+            if(line.indexOf('\t') != -1) {
+                if (separator != -1 && line.indexOf('\t') < separator){
+                    separator = line.indexOf('\t');
+                }else if (separator == -1){
+                    separator = line.indexOf('\t');
+                }
+            }
+
+            // Add IP and Domain Name to hashmap
+            String ip = line.substring(0, separator);
+            ip = ip.trim();
+            String domainName = line.substring(separator+1);
+            domainName = domainName.trim();
+
+            dnsTable.put(domainName, ip);
 
         }
 
