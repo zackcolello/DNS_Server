b3690a82f02be14b92a0c626da9fe7b7e07741b4
Daivik Sheth <Daivik95@hotmail.com>
2016-03-31 15:49:27 -0400

    One argument check

--- src/com/IT/dns_server.java
+++ src/com/IT/dns_server.java
@@ -3,14 +3,18 @@ package com.IT;
 public class dns_server {
 
     public static void main(String[] args) {
-
-        System.out.println("Initial Test");
         System.out.println("Daivik Sheth");
 
+        int portNumber = 12345;
 
-        if(args.length == 2 && args[0].equals("-f")){
-
+        if(args.length == 1) {
+            if (args[0].equals("-p")) {
+                portNumber =Integer.parseInt(args[1]);
+            } else if (args[0].equals("-f")) {
+                
+            } else{
+                System.err.println("usage: java dns_server [-p port#] [-f hostfile]");
+            }
         }
-
     }
 }
